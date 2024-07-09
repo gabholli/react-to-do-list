2ebdd77f1218982923baa61d40c06788b5c648fd
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-07-08 21:39:46 -0700

    Update UseLocalStorage.tsx

--- src/hooks/UseLocalStorage.tsx
+++ src/hooks/UseLocalStorage.tsx
@@ -1,26 +1,21 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react"
 
-// Define the hook with a generic type parameter T
-const useLocalStorage = <T>(key: string, initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] => {
-    const [value, setValue] = useState<T>(() => {
+const UseLocalStorage = (key: string, initialValue: any) => {
+    const [value, setValue] = useState(() => {
         try {
-            const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
+            const localValue = window.localStorage.getItem(key)
+            return localValue ? JSON.parse(localValue) : initialValue
         } catch (err) {
-            console.log("Error reading from localStorage", err);
-        return initialValue;
+            console.log(err)
+            return initialValue
         }
-    });
+    })
 
     useEffect(() => {
-        try {
-            window.localStorage.setItem(key, JSON.stringify(value));
-        } catch (err) {
-            console.log("Error writing to localStorage", err);
-        }
-    }, [key, value]);
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }, [key, value])
 
-        return [value, setValue];
-};
+    return [value, setValue]
+}
 
-        export default useLocalStorage;
+export default UseLocalStorage
